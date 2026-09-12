a351abcbeb7edb1f0877aa34346c6398e7e4a177
sobat-2930 <sobatbintemannan29@gmail.com>
2026-09-12 00:03:53 -0700

    Add responsive TechGrid to render all technology cards

---
+++ src/components/TechGrid.tsx
@@ -0,0 +1,27 @@
+import TechnologyCard from "./TechnologyCard";
+import type { Technology } from "../types";
+
+interface TechGridProps {
+  technologies: Technology[];
+  stack: Technology[];
+  onAdd: (tech: Technology) => void;
+}
+
+export default function TechGrid({
+  technologies,
+  stack,
+  onAdd,
+}: TechGridProps) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+      {technologies.map((tech) => (
+        <TechnologyCard
+          key={tech.id}
+          tech={tech}
+          isAdded={stack.some((item) => item.id === tech.id)}
+          onAdd={onAdd}
+        />
+      ))}
+    </div>
+  );
+}
